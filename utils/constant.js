c0ce47584b41c04ee39cd3476ca9c3dcc7a1dcf9
Saharat Paynok <saharat.paynok@gmail.com>
2022-09-15 23:45:39 +0700

    first commit

---
+++ utils/constant.js
@@ -0,0 +1,20 @@
+const ASSET_TYPES = [
+  {
+    name: 'Condo',
+    value: 'condo',
+  },
+  {
+    name: 'House',
+    value: 'house',
+  },
+  {
+    name: 'Town House',
+    value: 'townhouse',
+  },
+  {
+    name: 'Land',
+    value: 'land',
+  },
+];
+
+export { ASSET_TYPES };
