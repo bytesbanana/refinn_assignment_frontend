baaf730fcc10d54b535069823fd8a43a1bd353b3
Saharat Paynok <saharat.paynok@gmail.com>
2022-09-16 02:01:49 +0700

    adding temporary nav

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <StyledEngineProvider injectFirst>
+      <div className=' bg-sky-500 w-full h-12 drop-shadow-md shadow-md flex justify-between items-center'>
+        <div className='p-2 text-white text-2xl font-bold'>Logo</div>
+      </div>
       <Component {...pageProps} />
     </StyledEngineProvider>
   );
